aeeafca030e5f30c22fc9305518b0f72b7a52ead
Jark6 <71470769+Jark6@users.noreply.github.com>
2021-04-06 18:15:15 +0500

    Add files via upload

---
+++ Java/Lab9/1/example2.java
@@ -0,0 +1,13 @@
+public class example2 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0"); //1. печатает "0"
+            throw new RuntimeException("Непроверяемая ошибка");
+            //System.out.println("1"); - эта часть кода не выполняется
+        }
+        catch (Exception e) {
+            System.out.println("2 "+ e ); //2. исключение перехвачено обработано, печатает "1" и исключение
+        }
+        System.out.println("3"); //3. печатает "3"
+    }
+}
---
+++ Java/Lab9/1/example8.java
@@ -0,0 +1,12 @@
+public class example8 {
+    public static int m(){
+        try {
+            System.out.println("0"); //1. печатает "0"
+            throw new RuntimeException(); // исключение не будет перехвачано, т.к. нет соответствующего catch, будет выведена ошибка
+        }
+        finally { System.out.println("1"); //2. "1" печатается не смотря на то, что исключение не перехватывается
+        }
+    }
+    public static void main(String[] args) { System.out.println(m());
+    }
+}
