9fbc9cae23c217687a6326518d0ba78fd521d131
Jark6 <71470769+Jark6@users.noreply.github.com>
2021-04-08 23:35:03 +0500

    Add files via upload

---
+++ Java/Lab7/Lab7_08_03/Lab7_08_03.java
@@ -0,0 +1,19 @@
+package Lab7_08_03;
+
+//import Lab7_08_01.superClassTest;
+
+public class Lab7_08_03 {
+    public static void main(String[] args) {
+        FirstSupClass superClassTest = new FirstSupClass(512);
+        String className1 = superClassTest.toString();
+        System.out.println(className1);
+
+        SecondSubClass FirstSubClassTest = new SecondSubClass(1024,'K');
+        String className2 = FirstSubClassTest.toString();
+        System.out.println(className2);
+
+        ThirdSubClass SecondSubClassTest = new ThirdSubClass(1024,'K',"кибибайт");
+        String className3 = SecondSubClassTest.toString();
+        System.out.println(className3);
+    }
+}
---
+++ Java/Lab7/Lab7_08_05/FirstSubClass.java
@@ -0,0 +1,34 @@
+/*
+в одном из классов появляется защищенное целочисленное поле,
+там есть конструктор с двумя параметрами
+и переопределен метод для отображения значений полей объекта и названия класса
+ */
+package Lab7_08_05;
+
+//на основе суперкласса создается подкласс
+public class FirstSubClass extends FirstSupClass {
+
+    //защищенное целочисленное поле
+    protected int int1;
+
+    public void setInt1(int int1) {
+        this.int1 = int1;
+    }
+//конструктор с двумя параметрами
+    FirstSubClass(String str1, int int1) {
+        super(str1);
+        this.setInt1(int1);
+        }
+
+        //переопределен метод для отображения значений полей объекта и названия класса
+    @Override
+    public void PrintToString() {
+        String SecondFieldValue =
+                " int1 = \"" + this.getInt1()+"\"";
+        super.PrintToString();
+        System.out.println(SecondFieldValue);
+    }
+    public int getInt1() {
+        return int1;
+    }
+}
